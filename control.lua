5c8e069db405d256f9ee0ec7ac1a565e1669acdc
killahkik <114204994+killahkik@users.noreply.github.com>
2024-12-12 09:09:22 -0500

    hide surfaces, fix cam zoom/pos, fix placing higher shape entities, add equipment surface endpoint clearing and storing data

--- control.lua
+++ control.lua
@@ -1,6 +1,8 @@
--- TODO: block adding ghost equipment (check if the added equipment is a ghost and if so, delete the ghost entity and return)
--- TODO: when taking off armor, if there is no new armor on remove surface entities
--- TODO: turn off game prints
+-- TODO: block adding ghost equipment (check if the added equipment is a ghost and if true AND it's a minifactory piece of equipment, delete the ghost entity and return)
+-- check if the equipment type is ghost, if true, return
+-- TODO: return when adding non-minifactory equipment-- check if the equipment name starts with "minifactory" and if false, return
+-- TODO: when taking off armor, if there is no new armor on, remove surface entities-- use checkPlayerGrid to check if the grid is valid, if false, return
+-- TODO: hide equipment surfaces from remote view, use a button to view them instead -- luaForce.set_surface_hidden(surface, hidden)
 
 -- returns the surface name for the player's equipment surface, creating it if it doesn't exist
 local function getOrMakePlayerSurfaceName(player_index)
@@ -9,12 +11,14 @@ local function getOrMakePlayerSurfaceName(player_index)
         game.create_surface(surfaceName, {width = 200, height = 200, no_enemies_mode = true, default_enable_all_autoplace_controls = false, autoplace_controls = {}})
         game.get_surface(surfaceName).always_day = true
     end
+    local surface = game.surfaces[surfaceName]
+    game.players[player_index].force.set_surface_hidden(surface, true)
     return surfaceName
 end
 
 local function printEquipmentGridContents(grid)
     for _, equipment in pairs(grid.equipment) do
-        game.print(equipment.name .. " at position " .. equipment.position.x .. ", " .. equipment.position.y)
+        --game.print(equipment.name .. " at position " .. equipment.position.x .. ", " .. equipment.position.y)
     end
 end
 
@@ -32,9 +36,9 @@ local function clearAndPrepareSurface(surface)
 end
 
 local function deleteEntity(position, surface)
-    game.print("removing entities from surface " .. surface.name .. " at position " .. position.x .. ", " .. position.y)
+    --game.print("removing entities from surface " .. surface.name .. " at position " .. position.x .. ", " .. position.y)
     for _, entity in pairs(surface.find_entities_filtered{area = {{position.x, position.y}, {position.x + 1, position.y + 1}}}) do
-        game.print("destroying entity " .. entity.name)
+        --game.print("destroying entity " .. entity.name)
         entity.destroy()
     end
 end
@@ -50,13 +54,16 @@ local function updateGUI(player_index, grid)
         return
     end
     local gui = player.gui
-    -- TODO: test --> scale camzoom based on grid width and height (max of either)
+    -- scale camzoom based on grid width and height (max of either)
     local maxGridDimension = math.max(grid.width, grid.height)
-    local camZoom = maxGridDimension / 20
-    -- TODO: test --> move cam to center of grid coordinates
+    -- higher camzoom means more zoomed in
+    local camZoom = 5 / maxGridDimension
+    --game.print("camZoom: " .. camZoom)
+    -- move cam to center of grid coordinates
     local camX = grid.width / 2
     local camY = grid.height / 2
     local camPosition = {camX, camY}
+    --game.print("camPosition: " .. camX .. ", " .. camY)
     -- create frame and cam if there is none
     local root = gui.relative
     if root.equipment_camera_frame == nil then
@@ -69,8 +76,10 @@ local function updateGUI(player_index, grid)
     else
         --update cam zoom
         root.equipment_camera_frame.equipment_camera.zoom = camZoom
+        --update cam position
+        root.equipment_camera_frame.equipment_camera.position = camPosition
     end
-    -- TODO:
+    -- TODO: add button to view player's equipment surface
 end
 
 local function createSurfaceEntities(surface, grid, player)
@@ -80,9 +89,9 @@ local function createSurfaceEntities(surface, grid, player)
         -- if entity with name doesnt exist, skip
         local entityPrototypes = prototypes.get_entity_filtered{{filter = "name", name = name}}
         if #(entityPrototypes) == 0 then
-            game.print("Entity " .. name .. " does not exist to place on surface " .. surface.name)
+            --game.print("Entity " .. name .. " does not exist to place on surface " .. surface.name)
         else
-            local entity = surface.create_entity{name = name, position = position, force = player.force}
+            surface.create_entity{name = name, position = position, force = player.force}
         end
     end
 end
@@ -115,10 +124,49 @@ local function updateEquipmentSurface(playerIndex)
     player.force.chart_all(surface)
 end
 
-local function updateEquipmentSurfaceEndpoints(player_index)
-    -- TODO: find endpoint entitity (iron chest) on player's equipment surface and get and store the item count, then clear their inv
+local function updateEquipmentSurfaceEndpoints(player_index) -- TODO: look over this function, make sure its correct
     local surface = game.get_surface(getOrMakePlayerSurfaceName(player_index))
-    -- TODO: store endpoint item counts in dictionaries for each player- # of each item. then use that to update player buffs
+    if surface == nil then
+        return
+    end
+    local endpoint = surface.find_entities_filtered{type = "container"}
+    if #endpoint == 0 then
+        return
+    end
+    local endpointInventory = endpoint[1].get_inventory(defines.inventory.chest)
+    local player = game.get_player(player_index)
+    if storage.playerGrids[player_index].endpointItems == nil then
+        storage.playerGrids[player_index].endpointItems = {}
+    end
+    if endpointInventory == nil then
+        return
+    end
+    if endpointInventory.is_empty() == false then
+        local items = endpointInventory.get_contents()
+        table.insert(storage.playerGrids[player_index].endpointItems, items)
+        for _, item in pairs(items) do
+            --game.print("added items to endpointItems" .. player_index .. ": " .. item.name .. " " .. item.count)
+        end
+        endpointInventory.clear()
+    else
+        table.insert(storage.playerGrids[player_index].endpointItems, {})
+        game.print("added empty table to endpointItems" .. player_index)
+    end
+    -- ensure endpointItems table does not exceed 10 elements
+    if #storage.playerGrids[player_index].endpointItems > 10 then
+        local removed = table.remove(storage.playerGrids[player_index].endpointItems, 1)
+        for _, item in pairs(removed) do
+            --game.print("removed element from endpointItems" .. player_index .. ": " .. item.name .. " " .. item.count)
+        end
+    end
+end
+
+local function getTotalEndpoint()
+
+end
+
+local function updatePlayerBuffs(player_index) -- TODO: implement
+
 end
 
 local function checkPlayerGrid(grid)
@@ -135,11 +183,11 @@ local function savePlayerGrid(grid, playerIndex)
     -- save player's grid to storage table for later comparison
     if playerIndex == nil then
         playerIndex = grid.player_owner.index
-        game.print("playerIndex is nil, setting to " .. playerIndex)
+        --game.print("playerIndex is nil, setting to " .. playerIndex)
     end
     if storage.playerGrids == nil then
         storage.playerGrids = {}
-        game.print("created playerGrids table")
+        --game.print("created playerGrids table")
     end
     if storage.playerGrids[playerIndex] == nil then
         storage.playerGrids[playerIndex] = {}
@@ -156,7 +204,7 @@ local function savePlayerGrid(grid, playerIndex)
         local equipmentCopy = {position = position, name = name}
         table.insert(storage.playerGrids[playerIndex].equipment, equipmentCopy)
     end
-    game.print("saved grid at storage.playerGrids[" .. playerIndex .. "], new count: " .. #storage.playerGrids[playerIndex].equipment)
+    --game.print("saved grid at storage.playerGrids[" .. playerIndex .. "], new count: " .. #storage.playerGrids[playerIndex].equipment)
     printEquipmentGridContents(grid)
 end
 
@@ -164,9 +212,9 @@ local function getPositionsOfRemovedEquipment(grid, lastGrid)
     -- compare player's last saved grid to current grid to find removed equipment
     local positions = {}
     -- compare grid.equipment and lastGrid.equipment to find removed equipment- for each position in lastGrid.equipment, if it doesnt exist in grid.equipment, add to positions
-    game.print("count of equipment in lastGrid: " .. #lastGrid.equipment)
+    --game.print("count of equipment in lastGrid: " .. #lastGrid.equipment)
     printEquipmentGridContents(lastGrid)
-    game.print("count of equipment in grid: " .. #grid.equipment)
+    --game.print("count of equipment in grid: " .. #grid.equipment)
     printEquipmentGridContents(grid)
     for _, equipment in pairs(lastGrid.equipment) do
         local position = equipment.position
@@ -175,7 +223,7 @@ local function getPositionsOfRemovedEquipment(grid, lastGrid)
         for _, currentEquipment in pairs(grid.equipment) do
             if currentEquipment.position.x == position.x and currentEquipment.position.y == position.y and currentEquipment.name == name then
                 removed = false
-                game.print("equipment not removed, position: " .. position.x .. ", " .. position.y)
+                --game.print("equipment not removed, position: " .. position.x .. ", " .. position.y)
                 break
             end
         end
@@ -187,12 +235,12 @@ local function getPositionsOfRemovedEquipment(grid, lastGrid)
 end
 
 local function getPositionsOfAddedEquipment(grid, lastGrid)
-    game.print("getting positions of added equipment")
+    --game.print("getting positions of added equipment")
     -- compare player's last saved grid to current grid to find added equipment
     local positions = {}
-    game.print("count of equipment in lastGrid: " .. #lastGrid.equipment)
+    --game.print("count of equipment in lastGrid: " .. #lastGrid.equipment)
     printEquipmentGridContents(lastGrid)
-    game.print("count of equipment in grid: " .. #grid.equipment)
+    --game.print("count of equipment in grid: " .. #grid.equipment)
     printEquipmentGridContents(grid)
     -- compare grid.equipment and lastGrid.equipment to find added equipment- for each position in grid.equipment, if it doesnt exist in lastGrid.equipment, add to positions
     for _, equipment in pairs(grid.equipment) do
@@ -202,7 +250,7 @@ local function getPositionsOfAddedEquipment(grid, lastGrid)
         for _, lastEquipment in pairs(lastGrid.equipment) do
             if lastEquipment.position.x == position.x and lastEquipment.position.y == position.y and lastEquipment.name == name then
                 added = false
-                game.print("equipment not added, position: " .. position.x .. ", " .. position.y)
+                --game.print("equipment not added, position: " .. position.x .. ", " .. position.y)
                 break
             end
         end
@@ -214,9 +262,10 @@ local function getPositionsOfAddedEquipment(grid, lastGrid)
 end
 
 local function onAddEquipment(event)
-    game.print("adding equipment")
+    --game.print("adding equipment")
     local grid = event.grid
     local entity = event.equipment.name
+    local shape = event.equipment.shape
     if checkPlayerGrid(grid) == false then
         return
     end
@@ -229,21 +278,36 @@ local function onAddEquipment(event)
     -- check positions has at least one element and add new entities to surface
     if #positions > 0 then
         for _, position in pairs(positions) do
-            game.print("adding entity to surface " .. getOrMakePlayerSurfaceName(playerIndex) .. " at position: " .. position.x .. ", " .. position.y)
+            --game.print("adding entity to surface " .. getOrMakePlayerSurfaceName(playerIndex) .. " at position: " .. position.x .. ", " .. position.y)
+            -- make sure it's placed at top left corner according to shape (since it seems to place at bottom right default)
+            if shape.width > 1 then
+                local count = shape.width
+                while count > 1 do
+                    position.x = position.x + 1
+                    count = count - 1
+                end
+            end
+            if shape.height > 1 then
+                local count = shape.height
+                while count > 1 do
+                    position.y = position.y + 1
+                    count = count - 1
+                end
+            end
             addEntity(position, game.get_surface(getOrMakePlayerSurfaceName(playerIndex)), entity, player)
         end
     else
-        game.print("no positions to add")
+        --game.print("no positions to add")
     end
 end
 
 
 
 local function onRemoveEquipment(event)
-    game.print("removing equipment")
+    --game.print("removing equipment")
     local grid = event.grid
     if checkPlayerGrid(grid) == false then
-        game.print("not player grid")
+        --game.print("not player grid")
         return
     end
     local playerIndex = event.grid.player_owner.index
@@ -257,7 +321,7 @@ local function onRemoveEquipment(event)
             deleteEntity(position, game.get_surface(getOrMakePlayerSurfaceName(playerIndex)))
         end
     else
-        game.print("no positions to remove, positions = " .. #positions)
+        --game.print("no positions to remove, positions = " .. #positions)
     end
 end
 
@@ -278,8 +342,8 @@ local function onChangeArmor(event)
 end
 
 local function onTick(event)
-    local seconds = 5
-    if event.tick % (60 * seconds) == 0 then
+    local endpointsSeconds = 5
+    if event.tick % (60 * endpointsSeconds) == 0 then
         for _, player in pairs(game.players) do
             updateEquipmentSurfaceEndpoints(player.index)
             if game.get_surface(getOrMakePlayerSurfaceName(player.index)) ~= nil then
@@ -294,7 +358,6 @@ script.on_event(defines.events.on_equipment_inserted, onAddEquipment)
 script.on_event(defines.events.on_equipment_removed, onRemoveEquipment)
 script.on_event(defines.events.on_player_armor_inventory_changed, onChangeArmor)
 
--- every xth tick
 script.on_event(defines.events.on_tick, onTick)
 
 -- TODO: add update proofing to GUI, see https://github.com/ClaudeMetz/UntitledGuiGuide/wiki/Chapter-8:-Going-With-the-Times
